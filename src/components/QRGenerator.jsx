e5b2f4a2dfaa5d40727192324c309ce578c1ef55
calvin <calvinhaviandy@gmail.com>
2025-05-21 01:12:31 +0700

    fix : qrgenartor

--- src/components/QRGenerator.jsx
+++ src/components/QRGenerator.jsx
@@ -1,9 +1,16 @@
 import QRCode from "react-qr-code";
+
 export default function QRGenerator({ data }) {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "2rem",
+      }}>
       <h3>QR Code Absensi:</h3>
-      <QRCode value={data} size={256} />
+      <QRCode value={data} size={300} />
     </div>
   );
 }
